9ccc79dc1e0d81ff072ab654044a2020efe5c3b1
Arthaud <arthaudp33@gmail.com>
2024-01-23 16:05:11 +0100

    feat: basic homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,5 @@
 <script lang="ts">
-    import { loadModel, placeModel } from "$lib/functions";
-    import { createScene } from "$lib/three";
-    import { onMount } from "svelte";
-    import type { GLTF } from "three/examples/jsm/loaders/GLTFLoader.js";
-
-    let canvas; 
-
-    onMount(() => {
-        createScene(canvas)
-        loadModel('/assets/Avion.glb').then((model: GLTF) => {
-            placeModel(model)
-        })
-        loadModel('/assets/Island.glb').then((model: GLTF) => {
-            placeModel(model)
-        })
-        loadModel('/assets/scene.glb').then((model: GLTF) => {
-            placeModel(model)
-        })
-    })
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<canvas id="three" bind:this={canvas}></canvas>
+<h1>Olena</h1>
+<a href="/map">Voir la carte</a>
