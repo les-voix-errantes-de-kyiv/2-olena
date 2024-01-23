0e07bc4292df37fff92d1eb92529d87a8fdb050b
Emma Guillaume <emma.guillaume@mmibordeaux.com>
2024-01-23 14:21:29 +0100

    [FEAT] - toggle between two rooms in same page

--- src/lib/rooms.ts
+++ src/lib/rooms.ts
@@ -2,63 +2,51 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-export const createRoomBeforeScene = async (canvas: HTMLCanvasElement) => {
+export type RoomScene = {
+	controls: OrbitControls;
+	camera: THREE.PerspectiveCamera;
+	scene: THREE.Scene;
+	renderer: THREE.WebGLRenderer;
+	clock: THREE.Clock;
+	showBeforeRoom: () => void;
+	showAfterRoom: () => void;
+};
+
+//mettre une autre propos avec la variable dans la page svelte ?????
+export const createRoomScene = async (canvas: HTMLCanvasElement): Promise<RoomScene> => {
 	const gltfLoader = new GLTFLoader();
 	// Scene
 	const scene = new THREE.Scene();
 
-	/**
-	 * Textures
-	 */
+	const ambientLight = new THREE.AmbientLight('#ffffff', 0.01);
 
-	/**
-	 * Lights
-	 */
-	// Ambient light
-	const ambientLight = new THREE.AmbientLight('#ffffff', 0.5);
-	scene.add(ambientLight);
-
-	// Directional light
-	const sunLight = new THREE.DirectionalLight('#FFC371', 2.5);
+	// LIGHT
+	const sunLight = new THREE.DirectionalLight('#FFC371', 1.5);
 	sunLight.position.set(0.5, 2, -2.5);
-	scene.add(sunLight);
+
+	const ambientLightAfter = new THREE.AmbientLight('#4C6561', 1);
+
+	const sunLightAfter = new THREE.DirectionalLight('#B9C9CB', 2);
+	sunLightAfter.position.set(0.5, 2, -2.5);
 
 	const lampLight = new THREE.PointLight(0xff9000, 0.5);
 	lampLight.position.set(-1.6, 0.9, 1.35);
-	scene.add(lampLight);
 
 	const candleLight = new THREE.PointLight(0xff9000, 0.2);
 	candleLight.position.set(0.05, 0.6, -0.32);
-	scene.add(candleLight);
 
-	const directionalLightHelper = new THREE.DirectionalLightHelper(sunLight, 0.2);
-	scene.add(directionalLightHelper);
-
-	// const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
-	// scene.add(hemisphereLightHelper)
-
-	const lampLightHelper = new THREE.PointLightHelper(lampLight, 0.2);
-	scene.add(lampLightHelper);
-
-	const candleHelper = new THREE.PointLightHelper(candleLight, 0.2);
-	scene.add(candleHelper);
-
-	// const spotLightHelper = new THREE.SpotLightHelper(spotLight)
-	// scene.add(spotLightHelper)
-
+	// AXES
 	const axesHelper = new THREE.AxesHelper(5);
 	scene.add(axesHelper);
 
 	const gridHelper = new THREE.GridHelper(5, 5);
 	scene.add(gridHelper);
 
-	// objects
+	// OBJECTS
 	const room = (await gltfLoader.loadAsync('/assets/gltf/room/piecev1.gltf')).scene;
 	room.scale.set(2, 2, 2);
 	scene.add(room);
-	/**
-	 * Sizes
-	 */
+
 	const sizes = {
 		width: window.innerWidth,
 		height: window.innerHeight
@@ -78,10 +66,7 @@ export const createRoomBeforeScene = async (canvas: HTMLCanvasElement) => {
 		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 	});
 
-	/**
-	 * Camera
-	 */
-	// Base camera
+	// CAMERA
 	const camera = new THREE.PerspectiveCamera(80, sizes.width / sizes.height, 0.1, 100);
 	camera.position.set(0.75, 2, 0.75);
 
@@ -112,13 +97,37 @@ export const createRoomBeforeScene = async (canvas: HTMLCanvasElement) => {
 	 */
 	renderer.shadowMap.enabled = true;
 
+	const groupBeforeRoom = new THREE.Group();
+	groupBeforeRoom.add(ambientLight, sunLight, lampLight, candleLight);
+	scene.add(groupBeforeRoom);
+
+	const groupAfterRoom = new THREE.Group();
+	groupAfterRoom.add(ambientLightAfter, sunLightAfter);
+	scene.add(groupAfterRoom);
+
+	scene.add(groupBeforeRoom);
+	scene.add(groupAfterRoom);
+
+	const showBeforeRoom = () => {
+		groupBeforeRoom.visible = true;
+		groupAfterRoom.visible = false;
+	};
+
+	const showAfterRoom = () => {
+		groupBeforeRoom.visible = false;
+		groupAfterRoom.visible = true;
+	};
+
+	showAfterRoom();
+
 	/**
 	 * Animate
 	 */
-	// const clock = new THREE.Clock();
+	const clock = new THREE.Clock();
 
 	const tick = () => {
-		// const elapsedTime = clock.getElapsedTime();
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const elapsedTime = clock.getElapsedTime();
 
 		// Update controls
 		controls.update();
@@ -131,115 +140,6 @@ export const createRoomBeforeScene = async (canvas: HTMLCanvasElement) => {
 	};
 
 	tick();
-};
 
-export const createRoomAfterScene = async (canvas: HTMLCanvasElement) => {
-	const gltfLoader = new GLTFLoader();
-	// Scene
-	const scene = new THREE.Scene();
-
-	/**
-	 * Textures
-	 */
-
-	/**
-	 * Lights
-	 */
-	// Ambient light
-	const ambientLight = new THREE.AmbientLight('#4C6561', 1);
-	scene.add(ambientLight);
-
-	// Directional light
-	const sunLight = new THREE.DirectionalLight('#B9C9CB', 2);
-	sunLight.position.set(0.5, 2, -2.5);
-	scene.add(sunLight);
-
-	const directionalLightHelper = new THREE.DirectionalLightHelper(sunLight, 0.2);
-	scene.add(directionalLightHelper);
-
-	const axesHelper = new THREE.AxesHelper(5);
-	scene.add(axesHelper);
-
-	const gridHelper = new THREE.GridHelper(5, 5);
-	scene.add(gridHelper);
-
-	// objects
-	const room = (await gltfLoader.loadAsync('/assets/gltf/room/piecev1.gltf')).scene;
-	room.scale.set(2, 2, 2);
-	scene.add(room);
-	/**
-	 * Sizes
-	 */
-	const sizes = {
-		width: window.innerWidth,
-		height: window.innerHeight
-	};
-
-	window.addEventListener('resize', () => {
-		// Update sizes
-		sizes.width = window.innerWidth;
-		sizes.height = window.innerHeight;
-
-		// Update camera
-		camera.aspect = sizes.width / sizes.height;
-		camera.updateProjectionMatrix();
-
-		// Update renderer
-		renderer.setSize(sizes.width, sizes.height);
-		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-	});
-
-	/**
-	 * Camera
-	 */
-	// Base camera
-	const camera = new THREE.PerspectiveCamera(80, sizes.width / sizes.height, 0.1, 100);
-	camera.position.set(0.75, 2, 0.75);
-
-	camera.lookAt(-3, 3, 0);
-	scene.add(camera);
-
-	// Controls
-	const controls = new OrbitControls(camera, canvas);
-	controls.minAzimuthAngle = -0.7;
-	controls.maxAzimuthAngle = 1.75;
-	controls.minPolarAngle = Math.PI / 4;
-	controls.maxPolarAngle = Math.PI / 2 - 0.4;
-	controls.enableZoom = false;
-	controls.enablePan = false;
-
-	/**
-	 * Renderer
-	 */
-	const renderer = new THREE.WebGLRenderer({
-		canvas: canvas
-	});
-	renderer.setClearColor('#ffffff');
-	renderer.setSize(sizes.width, sizes.height);
-	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-	/**
-	 * Shadows
-	 */
-	renderer.shadowMap.enabled = true;
-
-	/**
-	 * Animate
-	 */
-	// const clock = new THREE.Clock();
-
-	const tick = () => {
-		// const elapsedTime = clock.getElapsedTime();
-
-		// Update controls
-		controls.update();
-
-		// Render
-		renderer.render(scene, camera);
-
-		// Call tick again on the next frame
-		window.requestAnimationFrame(tick);
-	};
-
-	tick();
+	return { controls, camera, scene, renderer, clock, showBeforeRoom, showAfterRoom };
 };
---
+++ src/routes/rooms/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import { createRoomScene, type RoomScene } from '$lib/rooms';
+	import { onMount } from 'svelte';
+	import { get } from 'svelte/store';
+
+	let isBeforeRoomActive: boolean = true;
+
+	let canvas: HTMLCanvasElement;
+	let roomScene: RoomScene;
+	onMount(async () => {
+		// createRoomScene(canvas);
+		roomScene = await createRoomScene(canvas);
+	});
+</script>
+
+<body>
+	<button on:click={roomScene.showBeforeRoom}> avant </button>
+
+	<button on:click={roomScene.showAfterRoom}> apres </button>
+
+	<section>
+		<canvas class="invisible" id="three-before" bind:this={canvas}></canvas>
+	</section>
+</body>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	html,
+	body {
+		overflow: hidden;
+	}
+	.change-page {
+		position: absolute;
+		top: 1%;
+		right: 2%;
+		z-index: 100;
+		color: black;
+		border-radius: 10px;
+		background: white;
+		font-size: 16px;
+		text-decoration: none;
+		padding: 1rem;
+	}
+
+	/* @media screen and (min-width: 900px) {
+		section {
+			display: flex;
+			gap: 1rem;
+		}
+		.webgl1 {
+			max-width: 50vw;
+		}
+		.webgl2 {
+			max-width: 50vw;
+		}
+	} */
+</style>
