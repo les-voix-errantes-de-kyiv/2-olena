83073693170732d02c3d80ade80adc9d2fab1ef6
Emma Guillaume <emma.guillaume@mmibordeaux.com>
2024-01-24 09:25:57 +0100

    [FEAT] -  add ui lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		fontSize: {
+			base: '16px',
+			'2xl': '24px',
+			'4xl': '36px',
+			'6xl': '60px',
+		  },
+		fontFamily: {
+			'text': 'GothamBook',
+			'title': 'Gotham-Black',
+		},
+		extend: {
+			colors: {
+				'turquoise': '#00AEC7',				
+				'yellow': '#FCD56C',
+				'purple': '#A880DA',
+				'blue': '#023785',
+				'white': '#FFFFFF',
+				'black': '#1E1E1E',
+				'orange': '#FF8A35',
+			  },
+		},
 		container: {
 			center: true,
 			padding: {
