a6d5f7a73ebab701f262d4059a14b82fd1a17bcc
Arthaud <arthaudp33@gmail.com>
2024-01-25 12:35:01 +0100

    feat: read step infos in same page as map

--- src/lib/components/Menu.svelte
+++ src/lib/components/Menu.svelte
@@ -9,29 +9,29 @@
 {#if isMenuOpen}
 	<button
 		on:click={() => (isMenuOpen = false)}
-		class="fixed z-20 p-2 text-white border border-white rounded-full top-4 right-2 padding-4 hover:bg-white hover:text-purple"
+		class="padding-4 fixed right-2 top-4 z-20 rounded-full border border-white p-2 text-white hover:bg-white hover:text-purple"
 	>
 		<IconX />
 	</button>
-	<div class="fixed top-0 left-0 z-10 w-full h-screen bg-purple">
-		<nav class="container flex flex-col items-center justify-center h-full gap-4 text-white">
-			<a href="/" on:click={() => (isMenuOpen = false)} class="text-4xl font-title hover:underline">
+	<div class="fixed left-0 top-0 z-10 h-screen w-full bg-purple">
+		<nav class="container flex h-full flex-col items-center justify-center gap-4 text-white">
+			<a href="/" on:click={() => (isMenuOpen = false)} class="font-title text-4xl hover:underline">
 				Accueil
 			</a>
 
 			<a
 				href="/rooms"
 				on:click={() => (isMenuOpen = false)}
-				class="text-4xl font-title hover:underline"
+				class="font-title text-4xl hover:underline"
 			>
 				Chez Olena
 			</a>
 
-			<div class="flex flex-col gap-2 justify-center items-center">
+			<div class="flex flex-col items-center justify-center gap-2">
 				<a
 					href="/map"
 					on:click={() => (isMenuOpen = false)}
-					class="text-4xl font-title hover:underline"
+					class="font-title text-4xl hover:underline"
 				>
 					Le périple
 				</a>
@@ -43,10 +43,9 @@
 							isMenuOpen = false;
 							window.location.href = `/map?step=${index}`;
 						}}
-						class="mt-2 -mb-2 text-2xl font-text hover:underline"
-
+						class="-mb-2 mt-2 font-text text-2xl hover:underline"
 					>
-						{step.menuName}
+						{step.city}
 					</a>
 				{/each}
 			</div>
@@ -54,7 +53,7 @@
 			<a
 				href="/credits"
 				on:click={() => (isMenuOpen = false)}
-				class="text-4xl font-title hover:underline"
+				class="font-title text-4xl hover:underline"
 			>
 				Crédits
 			</a>
@@ -63,7 +62,7 @@
 {:else}
 	<button
 		on:click={() => (isMenuOpen = true)}
-		class="fixed z-20 p-2 bg-white border rounded-full top-4 right-2 padding-4 border-purple text-purple hover:bg-purple hover:text-white"
+		class="padding-4 fixed right-2 top-4 z-20 rounded-full border border-purple bg-white p-2 text-purple hover:bg-purple hover:text-white"
 	>
 		<IconBar3 />
 	</button>
