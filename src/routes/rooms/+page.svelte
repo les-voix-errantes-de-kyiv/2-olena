63f2f13b28927d5a53bd4bffb1f8c7056a6b7897
Emma Guillaume <emma.guillaume@mmibordeaux.com>
2024-01-25 09:25:46 +0100

    [FEAT] - add loader

--- src/routes/rooms/+page.svelte
+++ src/routes/rooms/+page.svelte
@@ -9,6 +9,7 @@
 
 	const isXPFinished = localStorage.getItem('isXPFinished') === 'true';
 	let isShowingBefore = false;
+	let isLoading = false;
 
 	const showAfterRoom = () => {
 		isShowingBefore = false;
@@ -21,7 +22,9 @@
 
 	onMount(async () => {
 		roomScene = new RoomScene({ canvas });
+		isLoading = true;
 		await roomScene.init();
+		isLoading = false;
 
 		if (isXPFinished) {
 			showAfterRoom();
@@ -36,7 +39,7 @@
 		<h1 class="text-4xl font-title">Chez Olena</h1>
 	</header>
 
-	<section class="fixed left-0 bottom-0 w-full">
+	<section class="fixed bottom-0 left-0 w-full">
 		<div class="container flex flex-col justify-between w-full gap-2 py-2">
 			{#if isXPFinished}
 				<a class="btn-secondary" href="/map">
@@ -58,6 +61,18 @@
 			{/if}
 		</div>
 	</section>
+	{#if isLoading === true}
+		<div class="absolute inset-0 flex items-center justify-center w-full h-full bg-white">
+			<div class="flex flex-col items-center justify-center">
+				<h2 class="text-xl font-text text-blue">Chargement...</h2>
+				<div class="relative w-8 h-8 border-4 rounded-full animate-spin border-purple">
+					<div
+						class="absolute top-[50%] left-[50%] z-50 w-5 h-1 border-4 border-white rounded-full"
+					></div>
+				</div>
+			</div>
+		</div>
+	{/if}
 
 	<canvas id="three-before" bind:this={canvas}></canvas>
 </container>
