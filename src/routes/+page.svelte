bacba1f1adabc8ae70ca6024378e07ef7c7ef8c6
Arthaud <arthaudp33@gmail.com>
2024-01-26 14:55:46 +0100

    feat: add seo + icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,14 @@
 	import ArrowRight from '$lib/components/icons/mini/arrow-right.svelte';
 </script>
 
-<section class="container flex flex-col justify-end h-full py-4 align-bottom font-text text-blue">
-	<img class="w-1/2 mb-4" src="/assets/images/petites_billes.svg" alt="" />
+<section class="font-text text-blue container flex h-full flex-col justify-end py-4 align-bottom">
+	<img class="mb-4 w-1/2" src="/assets/images/petites_billes.svg" alt="" />
 
 	<div class="mb-4">
-		<h1 class="mb-2 text-4xl font-title">Les 3600 km <br /> d’Olena</h1>
+		<h1 class="font-title mb-2 text-4xl">Les 3600 km <br /> d’Olena</h1>
 		<h2 class="mb-4 text-2xl">Une narration interactive</h2>
 		<p class="text-base">
-			Olena est une médecin urologue Ukrainienne âgée de 48 ans. A l’arrivée de la guerre dans son
+			Olena est une médecin urologue Ukrainienne âgée de 48 ans. À l’arrivée de la guerre dans son
 			pays, elle a dû abandonner tout ce qu’elle connaissait. S’en suit un périple de 5 étapes avant
 			d’arriver en France, un pays dont elle ignore tout de la culture et de son fonctionnement.
 		</p>
